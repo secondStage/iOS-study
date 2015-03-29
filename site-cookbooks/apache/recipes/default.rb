62635abf162f37b66b220d5983683593fec40fe1
mafuyu-kamono <kamomafu21@yahoo.co.jp>
2015-03-29 23:31:19 +0900

    Add apache and mysql template

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -14,3 +14,14 @@ service "httpd" do
   action [ :enable, :start ]
 end
 
+%w{
+  /etc/httpd/conf/httpd.conf
+  /etc/httpd/conf.d/virtual.conf
+  /var/www/html/index.php
+}.each do |tmp|
+  template tmp do
+    owner "root"
+    group "root"
+    mode 0644
+  end
+end
